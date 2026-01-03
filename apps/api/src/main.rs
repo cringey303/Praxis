e0e730b2d17e3b4642a175b3748c2ceb654ab590
cringey303 <lucas@the-b.org>
2026-01-03 15:35:48 -0800

    set up empty web app

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // setup logging (view SQL queries or errors in terminal)
+    tracing_subscriber::fmt::init();
 }
+
+// create empty web app and run 'root' if "/" homepage is visited
+let app = Router::new().route("/", get(root));
