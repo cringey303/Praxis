f34a881991ce8589d89011fc9284635e73894b86
cringey303 <lucas@the-b.org>
2026-01-04 17:27:17 -0800

    updated main.rs with new DB code

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -1,11 +1,29 @@
-use axum::{routing::get, Router};
+use axum::{routing::{get, post}, Router};
 use std::net::SocketAddr;
 use tower_http::cors::{CorsLayer, Any};
+use sqlx::postgres::PgPoolOptions;
+use dotenvy::dotenv;
+
+mod auth;
 
 #[tokio::main]
 async fn main() {
+    // load env variables
+    dotenv().ok();
     // setup logging (view SQL queries or errors in terminal)
     tracing_subscriber::fmt::init();
+
+    // --- Connect to Database --- //
+    // get database url saved in .env
+    let database_url = std::env::var("DATABASE_URL")
+    .expect("DATABASE_URL must be set");
+
+    // a pool is a group of open connections to the database
+    let pool = PgPoolOptions::new()
+        .max_connections(5) // only 5 during development
+        .connect(&database_url) // connect to Docker/Postgres
+        .await
+        .expect("Failed to connect to DB");
 
     let cors = CorsLayer::new()
         .allow_origin(Any)
@@ -15,7 +33,9 @@ async fn main() {
     // create empty web app and run 'root' if "/" homepage is visited
     let app = Router::new()
         .route("/", get(root))
-        .layer(cors);
+        .route("/auth/signup", post(auth::signup_handler)) // map /auth/signup to fn signup_handler in auth module
+        .layer(cors)
+        .with_state(pool); // injecting the DB pool
 
     //define route
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
