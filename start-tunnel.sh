528d892cf8a3f31bc2ba19e2bf26e722366abf79
cringey303 <lucas@the-b.org>
2026-01-23 10:23:35 -0600

    remove previous test files

--- start-tunnel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if ! command -v ngrok &> /dev/null; then
-    echo "ngrok could not be found. Installing via Homebrew..."
-    brew install ngrok/ngrok/ngrok
-fi
-
-echo "Starting ngrok tunnels..."
-ngrok start --all --config="$HOME/Library/Application Support/ngrok/ngrok.yml" --config=./ngrok.yml
